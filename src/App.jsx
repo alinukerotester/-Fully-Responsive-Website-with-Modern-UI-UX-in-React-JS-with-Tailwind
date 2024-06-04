aa22a7499bf8fdfcd32ee7d7451c8e6065b04ed1
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-06-04 17:18:02 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
-
-function App() {
+const App = () =>{
 
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-  )
-}
+    <ButtonGradient/>
+    </>
+  );
+};
 
-export default App
+export default App;
