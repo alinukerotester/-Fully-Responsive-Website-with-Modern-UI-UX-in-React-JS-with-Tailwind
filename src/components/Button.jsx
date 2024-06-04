a9fe7b71d85ff79f6b78b4677f2350361442feb5
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-06-04 17:41:14 +0300

    up

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import ButtonSvg from '../assets/svg/ButtonSvg';
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({className, href, onClick, children, px, white}) => {
+ const renderButton = () => (
+    <button>
+        <span>{children}</span>
+        {ButtonSvg(white)}
+    </button>
+ )
+
+ return renderButton();
+};
 
 export default Button
