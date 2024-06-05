148a78db3ed57d2328e5045a00a31c0e054c89d8
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-06-05 15:43:30 +0300

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import {brainWave} from "../assets";
 
+import {brainWave} from "../assets";
+import {navigation} from '../constants';
 const Header = () => {
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
@@ -10,9 +11,11 @@ const Header = () => {
         </a>
         <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
             <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-                1
-                2
-                3
+               {navigation.map((item) => (
+                <a key={item.id} href={item.url} className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}>
+                    {item.title}
+                </a>
+               ))}
             </div>
         </nav>
         </div>
